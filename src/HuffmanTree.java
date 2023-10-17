e443cf22a486ca19065e638ecf3074d0f4fae843
Plixelated <76276169+Plixelated@users.noreply.github.com>
2023-10-16 18:54:06 -0700

    Added Message Encoding/Decoding
    Added Message Encoding
    Added Message Decoding
    Started Code Cleanup

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -2,22 +2,36 @@ import java.util.Scanner;
 
 public class HuffmanTree {
 
+    public static String encodedMsg;
+    public static String decodedMsg;
     public static String[] codeTable = new String[28];
 
     public static void main(String[] args) {
         String input;
 
-        //System.out.println("Please Type A Sentence \n(Press Enter When Complete)");
-        //Scanner scnr = new Scanner(System.in);
-        //input = scnr.nextLine();
+        System.out.println("Please Type A Sentence \n(Press Enter When Complete)");
+        Scanner scnr = new Scanner(System.in);
+        input = scnr.nextLine();
 
-        input = "\nSusie says it is easy";
+        //input = "\nSusie says it is easy";
+        System.out.println("Input: " + input);
         PriorityQueue queue = new PriorityQueue(input.length());
+
+        //Adds values to queue
         queue = addToQueue(queue, input);
+        //Sorts Tress in Queue
         queue = sortTrees(queue);
-        encodeTable(queue);
-        printCodeTable();
-        decodeTable(queue);
+        //Creates reference to tree in queue
+        BinarySearchTree tree = queue.peek();
+
+        //Creates Code Table
+        codeTable = createCodeTable(tree.root);
+
+        encodedMsg = encodeMessage(input);
+        decodedMsg = decodeTable(tree.root, encodedMsg);
+
+        System.out.println("Encoded Message: " + encodedMsg);
+        System.out.println("Decoded Message: " + decodedMsg);
     }
 
     public static PriorityQueue addToQueue(PriorityQueue queue, String input){
@@ -71,27 +85,69 @@ public class HuffmanTree {
         return queue;
     }
 
-    private static void setCodeTable(){
-        //Fills code table
-        for (int i = 0; i < codeTable.length; i++){
-            if (i < codeTable.length-2) {
+    private static String[] fillTable(String[] table){
+        //Fills a table with alphabetical values
+        for (int i = 0; i < table.length; i++){
+            if (i < table.length-2) {
                 int charCode = 65 + i;
-                char letter = (char) charCode;
-                codeTable[i] = String.valueOf(letter);
+                table[i] = String.valueOf((char)charCode);
             }
             else{
-                if (i == codeTable.length-2) {
-                    codeTable[i] = " ";
+                if (i == table.length-2) {
+                    table[i] = " ";
                 }
                 else
-                    codeTable[i] = "\n";
+                    table[i] = "\n";
             }
         }
+
+        return table;
     }
-    private static void encodeTable(PriorityQueue queue){
-        setCodeTable();
-        Node root = queue.peek().root;
+
+    private static String[] purgeTable(String[] table) {
+        for(int i = 0; i < table.length; i++){
+            if (table[i].charAt(0) != '0' && table[i].charAt(0) != '1')
+                table[i] = null;
+        }
+
+        return table;
+    }
+
+    private static String[] createCodeTable(Node root){
+        codeTable = fillTable(codeTable);
         traversePreOrder(root);
+        codeTable = purgeTable(codeTable);
+
+        return codeTable;
+    }
+
+    private static String encodeMessage(String input){
+        input = input.toUpperCase();
+        StringBuilder encodedMessage = new StringBuilder();
+        char[] ref = new char[codeTable.length];
+        for (int i = 0; i < ref.length; i++){
+            if (i < ref.length-2) {
+                int charCode = 65 + i;
+                ref[i] = (char) charCode;
+            }
+            else{
+                if (i == ref.length-2) {
+                    ref[i] = ' ';
+                }
+                else
+                    ref[i] = '\n';
+            }
+        }
+        for(int i = 0; i < input.length(); i++){
+            char currentLetter = input.charAt(i);
+            for(int j = 0; j < ref.length; j++){
+                if (currentLetter == ref[j]){
+                    encodedMessage.append(codeTable[j]);
+                }
+            }
+        }
+
+        return encodedMessage.toString();
     }
 
     private static void traversePreOrder(Node root){
@@ -132,8 +188,30 @@ public class HuffmanTree {
 
     }
 
-    private static void decodeTable(PriorityQueue queue){
-        Node root = queue.peek().root;
+    private static String decodeTable(Node root, String input){
+        Node current = root;
+        StringBuilder decodedMessage = new StringBuilder();
+
+        for(int i=0; i < input.length(); i++) {
+            char currentValue = input.charAt(i);
+            if (currentValue == '0'){
+                current = current.leftChild;
+                if (current.leftChild == null && current.rightChild == null) {
+                    decodedMessage.append(current.data);
+                    current = root;
+                }
+            }
+            else if (currentValue == '1'){
+                current = current.rightChild;
+                if (current.leftChild == null && current.rightChild == null) {
+                    decodedMessage.append(current.data);
+                    current = root;
+                }
+            }
+
+        }
+
+        return decodedMessage.toString();
 
     }
 
@@ -145,9 +223,9 @@ public class HuffmanTree {
     }
 
     private static void printCodeTable(){
-        System.out.print("Encoded Message: ");
+        System.out.println("Code Table: ");
         for(int i = 0; i < codeTable.length; i++){
-            if (codeTable[i].charAt(0) == '0' || codeTable[i].charAt(0) == '1')
+            if (codeTable[i] != null)
                 System.out.print(codeTable[i] + " ");
         }
     }
